1feef48cb3bd5e80acfd736950f16f51e57f88b3
Aasish <gk.aasishkarki@gmail.com>
2026-01-05 15:38:26 +0545

    Login and Signup function with Database

--- nagar_app/urls.py
+++ nagar_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views.main_view import dashboard_view
-from .views.auth_view import signup_module,login_model
+from .views.auth_view import signup_module,login_module
 
 urlpatterns = [
     path('',dashboard_view,name='dashboard'),
     path('signup/',signup_module,name='signup'),
-    path('login/',login_model,name='login'),
+    path('login/',login_module,name='login'),
 ]
--- nagar_app/views/auth_view.py
+++ nagar_app/views/auth_view.py
@@ -1,7 +1,84 @@
+
 from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+
+def login_module(request):
+    errors = {}
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if not email:
+            errors['email'] = "Email field is Required"
+        elif not User.objects.filter(username=email).exists():
+            errors['email'] = "Email does not exist ! please signup first"
+
+        if not password:
+            errors['password'] = "Password is required"
+
+        if errors:
+            return render(request, 'auth/login.html', {'errors': errors,'data':request.POST})
+        
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        errors['username'] = "Invalid login credentials"
+        errors['password'] = "Invalid login credentials"
+        return render(request, 'auth/login.html', {'errors': errors,'data':request.POST})
 
 def signup_module(request):
-    return render(request,'auth/signup.html')
+    errors = {}
+    if request.method == 'POST':
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm = request.POST.get('confirm')
 
-def login_model(request):
-    return render(request,'auth/login.html')
+        # print(username,email,password,confirm)
+        if not firstname:
+            errors['firstname'] = "firstname is Required"
+
+        if not lastname:
+            errors['lastname'] = "lastname is Required"
+
+        if not email:
+            errors['email'] = "Email field is Required"
+        elif User.objects.filter(username=email).exists():
+            errors['email'] = "Email already exists ! user another email"
+        
+        if not password:
+            errors['password'] = "Password is required"
+        elif len(password) <=6:
+            errors['password']= "Password contains more than 6 characters"
+
+        if not confirm:
+            errors['confirm'] = " Confirm Password is required"
+
+        if password != confirm:
+            errors['confirm'] = "Confirm password not matched !!"
+
+        if errors:
+            return render(request, 'auth/signup.html', {'errors': errors,'data':request.POST})
+        
+        else:
+            user = User.objects.create_user(
+                username=email,
+                first_name=firstname,
+                last_name=lastname,
+                password=password,
+            )
+            messages.success(request,'User Registration Successfully !! ')
+            return redirect('login')
+
+        
+    else:
+        # first ma register page display huda
+        return render(request, 'auth/signup.html')
+
+    
