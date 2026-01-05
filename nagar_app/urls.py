e8c0c943c3afd612ff0131d258a7004776725d00
Aasish <gk.aasishkarki@gmail.com>
2026-01-05 15:06:54 +0545

    Signup Page Added

--- nagar_app/urls.py
+++ nagar_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views.main_view import dashboard_view
+from .views.auth_view import signup_module
 
 urlpatterns = [
-    path('',dashboard_view,name='dashboard')
+    path('',dashboard_view,name='dashboard'),
+    path('signup/',signup_module,name='signup'),
 ]
--- nagar_app/views/auth_view.py
+++ nagar_app/views/auth_view.py
@@ -1,1 +1,4 @@
 from django.shortcuts import render,redirect
+
+def signup_module(request):
+    return render(request,'auth/signup.html')
