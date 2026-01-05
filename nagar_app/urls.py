1c73d30c28379db3d0c8e43f09125863b7dca3fa
Aasish <gk.aasishkarki@gmail.com>
2026-01-05 14:11:17 +0545

    Integrating Tailwind CSS

--- nagar_app/urls.py
+++ nagar_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views.main_view import dashboard_view
 
 urlpatterns = [
-    
+    path('',dashboard_view,name='dashboard')
 ]
--- nagar_app/views/main_view.py
+++ nagar_app/views/main_view.py
@@ -1,1 +1,4 @@
 from django.shortcuts import render,redirect
+
+def dashboard_view(request):
+    return render(request, 'main/dashboard.html')
